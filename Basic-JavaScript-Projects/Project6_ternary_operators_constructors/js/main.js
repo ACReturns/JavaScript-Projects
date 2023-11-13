3c40c16b3acb3495bf86f776ad3f1c3950afb3b4
ACReturns <DSXtreme2012@gmail.com>
2023-11-12 22:46:59 -0800

    Ternary/ Operations/ Constructors
    - Created an age verification function with a select all function so the user can enter their age and easily clear the previous data

---
+++ Basic-JavaScript-Projects/Project6_ternary_operators_constructors/js/main.js
@@ -0,0 +1,15 @@
+function RideFunc()
+{
+    var Height, Can_ride;
+    Height = document.getElementById("Height").value;
+    Can_ride = (Height < 52) ? "You are too short" : "You are tall enough";
+    document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
+}
+
+function Verify_Age_Func()
+{
+    var Age, Eligible;
+    Age = document.getElementById("Age").value;
+    Eligible = (Age < 18) ? "You're too young" : "You're old enough";
+    document.getElementById("Eligible").innerHTML = Eligible + " to vote!";
+}
