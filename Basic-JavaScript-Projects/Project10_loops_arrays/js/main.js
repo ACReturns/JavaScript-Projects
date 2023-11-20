b1f32ccb49767b26bee57d37e15761f94341ae7b
ACReturns <DSXtreme2012@gmail.com>
2023-11-19 19:51:03 -0700

    Loops/ Arrays
    - examples for: for loop, array, constant, let

--- Basic-JavaScript-Projects/Project10_loops_arrays/js/main.js
+++ Basic-JavaScript-Projects/Project10_loops_arrays/js/main.js
@@ -32,3 +32,53 @@ function forLoop()
     document.getElementById("List_of_Instruments").innerHTML = Inventory;
 }
 //#endregion
+
+//#region Array
+function array_Func()
+{
+    var boroughs = [];
+    boroughs[0] = "Manhatten";
+    boroughs[1] = "Staten Island";
+    boroughs[2] = "Queens";
+    boroughs[3] = "Brooklyn";
+    boroughs[4] = "Bronx";
+
+
+    document.getElementById("Array").innerHTML = "From " + boroughs[1] + 
+    " to the " + boroughs[4] + ", New York has a blend of everything.";
+}
+//#endregion
+
+//#region Constant
+function constant_Func()
+{
+    const laserGun = {Model:"Moonraker", Maker:"Drax Industries", Owner:"Drax Henchmen"};
+    laserGun.Owner = "James Bond";
+    laserGun.Creator = "Hugo Drax";
+
+    document.getElementById("Constant").innerHTML = laserGun.Owner + " knocked out the henchmen and took the " + laserGun.Model
+    + " before making his way to face off against " + laserGun.Creator;
+}
+//#endregion
+
+//#region Let
+function let_Func()
+{
+    var turtles = "Shell";
+    document.write(turtles);
+    {
+        let turtles = "Green";
+        document.write(turtles);
+    }
+    document.write(turtles);
+}
+//#endregion
+
+//#region Return statement
+document.getElementById("Return").innerHTML = return_Func(document.getElementById("UserEntry").value)
+
+function return_Func(entry)
+{
+    return "Hi " + entry;
+}
+//#endregion
